168510f680f7a6b32eeacb868199a933b334ebfa
Stefan Syrett <stefansyrett17@gmail.com>
2024-12-05 16:18:25 +0000

    adding infra code

---
+++ infrastructure/main.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = var.app_service_plan_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku_name = var.app_service_sku
+  os_type             = var.app_service_os_type
+}
+
+resource "azurerm_windows_web_app" "wa" {
+  name                = var.app_service_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {}
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,35 @@
+variable "resource_group_name" {
+    description = "The name of the resource group in which the resources will be created."
+    type = string
+    default = "resource-group-01"
+}
+
+variable "location" {
+    description = "The Azure region in which the resources will be created."
+    type = string
+    default = "UK South"
+}
+
+variable "app_service_plan_name" {
+    description = "The name of the App Service Plan."
+    type = string
+    default = "app-service-plan-01"
+}
+
+variable "app_service_name" {
+    description = "The name of the App Service."
+    type = string
+    default = "app-service-01"
+}
+
+variable "app_service_sku" {
+    description = "The SKU of the App Service."
+    type = string
+    default = "F1"
+}
+
+variable "app_service_os_type" {
+    description = "The OS type of the App Service."
+    type = string
+    default = "Windows"
+}
