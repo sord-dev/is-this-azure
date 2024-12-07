f07743fd8d321dcff26ce233fa8a86afc9374d95
root <root@DESKTOP-7RN6BJD>
2024-12-07 12:56:11 +0000

    adding bash script to create actual artefact for deployment

---
+++ scripts/create-artefact.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create a temporary directory
+mkdir deployment
+
+# Copy files to the temporary directory
+cp application/dist/* deployment/ -r
+cp application/package.json deployment/
+cp application/package-lock.json deployment/
+
+# Zip all files in the temporary directory
+cd deployment
+zip -r release.zip .
+
+# Move the zip file to the root directory
+mv release.zip ../
